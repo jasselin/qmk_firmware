8c306bb0b386deaf24a6f52409a28db4bb807095
Jason Asselin <asselin.jason@gmail.com>
2022-02-20 08:59:01 -0500

    accents

--- keyboards/lily58/keymaps/jasselin/keymap.c
+++ keyboards/lily58/keymaps/jasselin/keymap.c
@@ -8,33 +8,52 @@ enum layer_number {
   _MEDIA = 4,
 };
 
+enum custom_keycodes {
+    ACC_AGRV,
+    ACC_ACRC,
+    ACC_EGRV,
+    ACC_ECRC,
+    ACC_CCED,
+    ACC_UCRC,
+    ACC_UGRV,
+    ACC_UTRM,
+    ACC_ICRC,
+    ACC_ITRM,
+    ACC_OCRC,
+    ACC_OTRM
+};
+
+#define CW_LEFT LGUI(LCTL(KC_LEFT))
+#define CW_RIGHT LGUI(LCTL(KC_RIGHT))
+#define CS_M LCTL(LSFT(KC_M))
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, 
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, OSL(_ACCENTS),
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, LCTL(LSFT(KC_M)),   KC_HOME, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, 
-                    KC_LGUI, KC_LALT, MO(2), LT(4,KC_NO),                KC_SPC, MO(3), KC_NO, KC_RGUI
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, CS_M,               KC_HOME, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, 
+                    KC_LGUI, KC_LALT, MO(2),   LT(4,KC_NO),                       KC_SPC,  MO(3),   KC_NO,   KC_RGUI
 ),
 [_ACCENTS] = LAYOUT(
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,  
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-                         KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO
+    KC_NO, KC_NO,    KC_NO, ACC_ECRC, KC_NO, KC_NO,                      KC_NO, ACC_UCRC, KC_NO,    KC_NO,    KC_NO, KC_NO,
+    KC_NO, ACC_ACRC, KC_NO, ACC_EGRV, KC_NO, KC_NO,                      KC_NO, ACC_UGRV, ACC_ICRC, ACC_OCRC, KC_NO, KC_NO,
+    KC_NO, ACC_AGRV, KC_NO, KC_NO,    KC_NO, KC_NO,                      KC_NO, ACC_UTRM, ACC_ITRM, ACC_OTRM, KC_NO, KC_TRNS,
+    KC_NO, KC_NO,    KC_NO, ACC_CCED, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, 
+                     KC_NO, KC_NO,    KC_NO, KC_NO,                      KC_NO, KC_NO,    KC_NO,    KC_NO
     ),
 [_FN] = LAYOUT(
-    LALT(KC_F4), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                                KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_PSCR, 
-    KC_NO, KC_NO, LCTL(KC_F4), LGUI(KC_UP), LCTL(KC_F5), KC_NO,                                    KC_NO, KC_F10, KC_F11, KC_F12, KC_NO, KC_PAUS, 
-    KC_NO, SGUI(KC_LEFT), LGUI(KC_LEFT), LGUI(KC_DOWN), LGUI(KC_RGHT), SGUI(KC_RGHT),              KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, 
-    KC_NO, LGUI(LCTL(KC_LEFT)), LGUI(KC_TAB), LGUI(LCTL(KC_RIGHT)), KC_NO, KC_NO,          KC_END, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-                                                    KC_NO, KC_NO, KC_TRNS, KC_NO,          KC_DEL, KC_WBAK, KC_WFWD, KC_NO),
+    LALT(KC_F4), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                                KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_PSCR, 
+    KC_NO,       KC_NO,         LCTL(KC_F4),   LGUI(KC_UP),   LCTL(KC_F5),   KC_NO,                KC_NO,  KC_F10,  KC_F11,  KC_F12,  KC_NO,   KC_PAUS, 
+    KC_NO,       SGUI(KC_LEFT), LGUI(KC_LEFT), LGUI(KC_DOWN), LGUI(KC_RGHT), SGUI(KC_RGHT),        KC_NO,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO, 
+    KC_NO,       CW_LEFT,       LGUI(KC_TAB),  CW_RIGHT,      KC_NO,         KC_NO,                KC_END, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, 
+                                KC_NO,         KC_NO,         KC_TRNS,       KC_NO,                        KC_DEL,  KC_WBAK, KC_WFWD, KC_NO),
 [_SYMWM] = LAYOUT(
-    LALT(KC_GRV), KC_NO, LALT(KC_2), KC_NO, KC_NO, KC_NO,                       KC_NO, KC_NO, KC_MINS, KC_EQL, KC_NO, KC_NO, 
-    HYPR(KC_GRV), HYPR(KC_Q), HYPR(KC_W), HYPR(KC_E), HYPR(KC_R), HYPR(KC_T),   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-    KC_NO, HYPR(KC_A), HYPR(KC_S), HYPR(KC_D), HYPR(KC_F), HYPR(KC_G),          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-    KC_NO, HYPR(KC_Z), HYPR(KC_X), HYPR(KC_C), HYPR(KC_V), HYPR(KC_B), KC_NO,     KC_NO, KC_NO, HYPR(KC_M), KC_NO, KC_NO, KC_NO, KC_NO, 
-    KC_NO, KC_NO, KC_NO, KC_NO,                                                   KC_NO, KC_TRNS, KC_NO, KC_NO),
+    LALT(KC_GRV), KC_NO,      LALT(KC_2), KC_NO,      KC_NO,      KC_NO,                        KC_NO, KC_NO,      KC_MINS, KC_EQL, KC_NO, KC_NO, 
+    HYPR(KC_GRV), HYPR(KC_Q), HYPR(KC_W), HYPR(KC_E), HYPR(KC_R), HYPR(KC_T),                   KC_NO, KC_NO,      KC_NO,   KC_NO,  KC_NO, KC_NO, 
+    KC_NO,        HYPR(KC_A), HYPR(KC_S), HYPR(KC_D), HYPR(KC_F), HYPR(KC_G),                   KC_NO, KC_NO,      KC_NO,   KC_NO,  KC_NO, KC_NO, 
+    KC_NO,        HYPR(KC_Z), HYPR(KC_X), HYPR(KC_C), HYPR(KC_V), HYPR(KC_B), KC_NO,     KC_NO, KC_NO, HYPR(KC_M), KC_NO,   KC_NO,  KC_NO, KC_NO, 
+                            KC_NO,        KC_NO,      KC_NO,      KC_NO,                        KC_NO, KC_TRNS,    KC_NO,   KC_NO),
 [_MEDIA] = LAYOUT(
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MUTE,                         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_VOLU,                         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
@@ -82,10 +101,54 @@ bool oled_task_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
-#ifdef OLED_ENABLE
-    set_keylog(keycode, record);
-#endif
+    #ifdef OLED_ENABLE
+        set_keylog(keycode, record);
+    #endif
+    //bool shiftPressed = (keyboard_report->mods & MOD_BIT (KC_LSFT)) || (keyboard_report->mods & MOD_BIT (KC_RSFT));
     // set_timelog();
+    switch (keycode) {
+        case ACC_AGRV:
+            SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_A));
+        break;
+        case ACC_ACRC:
+            SEND_STRING(SS_TAP(X_LBRACKET) SS_DELAY(100) SS_TAP(X_A));
+        break;
+        case ACC_EGRV:
+            SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_E));
+        break;
+        case ACC_ECRC:
+            SEND_STRING(SS_TAP(X_LBRACKET) SS_DELAY(100) SS_TAP(X_E));
+        break;
+        case ACC_CCED:
+            /*if (shiftPressed) {
+                SEND_STRING(SS_TAP(X_RBRACKET) SS_DELAY(100) SS_TAP(X_C));
+            }
+            else {*/
+                SEND_STRING(SS_TAP(X_RBRACKET) SS_DELAY(100) SS_TAP(X_C));
+            //}
+        break;
+        case ACC_UCRC:
+            SEND_STRING(SS_TAP(X_LBRACKET) SS_DELAY(100) SS_TAP(X_U));
+        break;
+        case ACC_UGRV:
+            SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_U));
+        break;
+        case ACC_UTRM:
+            SEND_STRING(SS_RSFT(SS_TAP(X_RBRACKET)) SS_DELAY(100) SS_TAP(X_U));
+        break;
+        case ACC_ICRC:
+            SEND_STRING(SS_TAP(X_LBRACKET) SS_DELAY(100) SS_TAP(X_I));
+        break;
+        case ACC_ITRM:
+            SEND_STRING(SS_RSFT(SS_TAP(X_RBRACKET)) SS_DELAY(100) SS_TAP(X_I));
+        break;
+        case ACC_OCRC:
+            SEND_STRING(SS_TAP(X_LBRACKET) SS_DELAY(100) SS_TAP(X_O));
+        break;
+        case ACC_OTRM:
+            SEND_STRING(SS_RSFT(SS_TAP(X_RBRACKET)) SS_DELAY(100) SS_TAP(X_O));
+        break;
+    }
   }
   return true;
 }
