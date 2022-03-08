72fcf9b8d9fd1ff8fee6c099ba004683922974bd
Jason Asselin <jason.asselin@cdid.com>
2022-03-08 12:52:11 -0500

    Add wm, media

--- keyboards/fauxlice/keymaps/default/keymap.c
+++ keyboards/fauxlice/keymaps/default/keymap.c
@@ -39,11 +39,11 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
-		KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,    KC_5, KC_6,         KC_7,   KC_8,  KC_9,    KC_0,    KC_MINS, KC_EQL,        KC_BSPC, 
-		KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,               KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC,       KC_RBRC, KC_BSLS, 
-		KC_ESC,  KC_A,    KC_S,    KC_D,   KC_F,    KC_G,               KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,       KC_BSPC, KC_ENT, 
-		KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,               KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, OSL(_ACCENTS), SH_HOME, 
-		KC_LCTL, KC_LGUI, KC_LALT, MO(_FN),                             KC_SPC, KC_NO, KC_LALT, KC_LCTL, KC_END),
+		KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,    KC_5, KC_6,         KC_7,   KC_8,       KC_9,    KC_0,    KC_MINS, KC_EQL,        KC_BSPC, 
+		KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,               KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,    KC_LBRC,       KC_RBRC, KC_BSLS, 
+		KC_ESC,  KC_A,    KC_S,    KC_D,   KC_F,    KC_G,               KC_H,   KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,       KC_BSPC, KC_ENT, 
+		KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,               KC_N,   KC_M,       KC_COMM, KC_DOT,  KC_SLSH, OSL(_ACCENTS), SH_HOME, 
+		KC_LCTL, KC_LGUI, KC_LALT, MO(_FN),                             KC_SPC, MO(_SYMWM), KC_RALT, KC_RCTL, LT(_MEDIA, KC_END)),
     [_ACCENTS] = LAYOUT(
 		RESET,  KC_NO,    KC_NO,   ACC_ECRC,  KC_NO,   KC_NO, KC_NO,       KC_NO,   ACC_UCRC,  KC_NO,       KC_NO,    KC_NO, KC_NO,       KC_NO, 
 		KC_NO,  ACC_ACRC, KC_NO,   ACC_EGRV,  KC_NO,   KC_NO,              KC_NO,   ACC_UGRV,  ACC_ICRC,    ACC_OCRC, KC_NO, KC_NO,       KC_NO, KC_NO, 
@@ -55,7 +55,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_NO,        KC_NO,       LCTL(KC_F4), WND_UP,   KC_NO,   KC_NO,              KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,   KC_PAUS, 
 		KC_NO,        SCR_LF,      WND_LF,      WND_DW,   WND_RG,  SCR_RG,             KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT, KC_NO,  KC_NO,  KC_NO,   KC_NO, 
 		KC_NO,        KC_NO,       CW_LF,       WND_DSK,  CW_RG,   KC_NO,              KC_WBAK, KC_WFWD, KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO, 
-		KC_NO,        KC_NO,       KC_NO,       KC_NO,                                 KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO)
+		KC_NO,        KC_NO,       KC_NO,       KC_NO,                                 KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO),
+    [_SYMWM] = LAYOUT(
+		LALT(KC_F4),  KC_NO,      RALT(KC_2),   KC_NO,       KC_NO,       KC_NO, KC_NO,       KC_NO,   KC_NO,  KC_NO,      KC_NO,    KC_NO, KC_NO, KC_NO, 
+		HYPR(KC_GRV), HYPR(KC_Q), HYPR(KC_W),   HYPR(KC_E),  HYPR(KC_R),  HYPR(KC_T),         KC_NO,   KC_NO,  KC_NO,      KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO,        HYPR(KC_A), HYPR(KC_S),   HYPR(KC_D),  HYPR(KC_F),  HYPR(KC_G),         KC_NO,   KC_NO,  KC_NO,      KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO,        HYPR(KC_Z), HYPR(KC_X),   HYPR(KC_C),  HYPR(KC_V),  HYPR(KC_B),         KC_NO,   KC_NO,  HYPR(KC_M), KC_NO,    KC_NO, KC_NO, KC_NO, 
+		KC_NO,        KC_NO,      KC_NO,        KC_NO,                                        KC_NO,   KC_NO,  KC_NO,      KC_NO,    KC_NO),
+    [_MEDIA] = LAYOUT(
+		KC_NO,  KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,       KC_NO,   KC_NO,  KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO, 
+		KC_NO,  KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,              KC_NO,   KC_NO,  KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO,  KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,              KC_NO,   KC_NO,  KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO,  KC_VOLD,  KC_MUTE, KC_VOLU, KC_MPRV, KC_MNXT,            KC_NO,   KC_NO,  KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO, 
+		KC_NO,  KC_NO,    KC_NO,   KC_MPLY,                              KC_NO,   KC_NO,  KC_NO,    KC_NO,    KC_NO)
     /*
     [_EMPTY] = LAYOUT(
 		KC_NO,  KC_NO,    KC_NO,   KC_NO,  KC_NO,   KC_NO, KC_NO,       KC_NO,   KC_NO,  KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO, 
